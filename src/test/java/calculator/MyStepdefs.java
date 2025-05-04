1d43025308e60298d1213fbf2c4dcb69858342f8
kasra.hajian <kasra.hajian@divar.ir>
2025-05-05 00:55:38 +0330

    Solve question 1

--- src/test/java/calculator/MyStepdefs.java
+++ src/test/java/calculator/MyStepdefs.java
@@ -1,16 +1,18 @@
 package calculator;
 
-import calculator.Calculator;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.Objects;
+
 public class MyStepdefs {
     private Calculator calculator;
     private int value1;
     private int value2;
+    private String opt;
     private int result;
 
     @Before
@@ -18,16 +20,46 @@ public class MyStepdefs {
         calculator = new Calculator();
     }
 
-    @Given("Two input values, {int} and {int}")
-    public void twoInputValuesAnd(int arg0, int arg1) {
+    @Given("Two input values, {int} and {int}, One operation +")
+    public void twoInputValuesFirstAndSecondOneOperationAdd(int arg0, int arg1) {
         value1 = arg0;
         value2 = arg1;
+        opt = "+";
     }
 
-    @When("I add the two values")
-    public void iAddTheTwoValues() {
-        result = calculator.add(value1, value2);
-        System.out.print(result);
+    @Given("Two input values, {int} and {int}, One operation *")
+    public void twoInputValuesFirstAndSecondOneOperationMultiply(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
+        opt = "*";
+    }
+
+    @Given("Two input values, {int} and {int}, One operation \\/")
+    public void twoInputValuesFirstAndSecondOneOperationDivide(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
+        opt = "/";
+    }
+
+    @Given("Two input values, {int} and {int}, One operation ^")
+    public void twoInputValuesFirstAndSecondOneOperationPower(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
+        opt = "^";
+    }
+
+    @When("I do the operation for the two values")
+    public void iOperateTheTwoValues() {
+        if (Objects.equals(opt, "*")) {
+            result = calculator.multiply(value1, value2);
+        } else if (Objects.equals(opt, "/")) {
+            result = calculator.divide(value1, value2);
+        } else if (Objects.equals(opt, "^")) {
+            result = calculator.power(value1, value2);
+        } else if (Objects.equals(opt, "+")) {
+            result = calculator.add(value1, value2);
+        }
+        System.out.println(value1 + " " + opt + " " + value2 + " = " + result);
     }
 
     @Then("I expect the result {int}")
